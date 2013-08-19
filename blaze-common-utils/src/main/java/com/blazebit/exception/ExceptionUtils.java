1f3a95a727e7e24d0abd1fe04cd2355de4f24514
Thomas Herzog <t.herzog@curecomp.com>
2013-08-19 09:26:26 +0200

    - Added getCause method to ExceptionUtils which searches for the causes which are given as varargs argument. The first occurring cause will be returned. - This method is usefull when someone needs to unwrapp the original cause within an unpredictable or unexpected exception wrapping such as AbortProcessException -> CoreException -> ObserverException -> WebBeansException -> InvocationTargetException -> InvocationTargetException -> SupplierNotFoundException - Added tests for this new method

--- blaze-common-utils/src/main/java/com/blazebit/exception/ExceptionUtils.java
+++ blaze-common-utils/src/main/java/com/blazebit/exception/ExceptionUtils.java
@@ -7,82 +7,122 @@ import java.lang.reflect.InvocationTargetException;
 
 /**
  * Utility class for exception objects.
- * 
+ *
  * @author Christian Beikov
+ * @author Thomas Herzog
  * @since 0.1.2
  */
 public final class ExceptionUtils {
 
-	private ExceptionUtils() {
-	}
+    private ExceptionUtils() {
+    }
 
-	/**
-	 * Unwraps the throwable until it is no instance of the throwableClass.
-	 * Basically this method unwraps the throwable instances by calling
-	 * getCause() except the throwable element is an instance of
-	 * InvocationTargetException. When an InvocationTargetException is given
-	 * getTargetException() is used for unwrapping.
-	 * 
-	 * Be carefull when you use this method with a too general throwableClass.
-	 * It might be, that getCause() returns the this element in some cases and
-	 * this could lead to an endless loop!
-	 * 
-	 * @param t
-	 *            The throwable to unwrap
-	 * @param throwableClass
-	 *            The class of which a throwable must be to be unwrapped
-	 * @return The unwrapped throwable or null if no further causes can be
-	 *         unwrapped
-	 */
-	@SafeVarargs
-	public static Throwable unwrap(Throwable t,
-			Class<? extends Throwable>... throwableClasses) {
-		Throwable unwrapped = t;
+    /**
+     * Unwraps the throwable until it is no instance of the throwableClass.
+     * Basically this method unwraps the throwable instances by calling
+     * getCause() except the throwable element is an instance of
+     * InvocationTargetException. When an InvocationTargetException is given
+     * getTargetException() is used for unwrapping.
+     *
+     * Be carefull when you use this method with a too general throwableClass.
+     * It might be, that getCause() returns the this element in some cases and
+     * this could lead to an endless loop!
+     *
+     * @param t              The throwable to unwrap
+     * @param throwableClass The class of which a throwable must be to be
+     *                       unwrapped
+     * @return The unwrapped throwable or null if no further causes can be
+     *         unwrapped
+     */
+    @SafeVarargs
+    public static Throwable unwrap(Throwable t,
+        Class<? extends Throwable>... throwableClasses) {
+        Throwable unwrapped = t;
 
-		while (unwrapped != null && isInstance(unwrapped, throwableClasses)) {
-			if (unwrapped.getCause() == null
-					&& unwrapped instanceof InvocationTargetException) {
-				// We do this here because an invocation target exception may
-				// return null on getCause() but the throwable element we are
-				// looking for can be accessed via getTargetException()
-				unwrapped = ((InvocationTargetException) unwrapped)
-						.getTargetException();
-			} else {
-				unwrapped = unwrapped.getCause();
-			}
-		}
+        while (unwrapped != null && isInstance(unwrapped, throwableClasses)) {
+            if (unwrapped.getCause() == null
+                && unwrapped instanceof InvocationTargetException) {
+                // We do this here because an invocation target exception may
+                // return null on getCause() but the throwable element we are
+                // looking for can be accessed via getTargetException()
+                unwrapped = ((InvocationTargetException) unwrapped)
+                    .getTargetException();
+            } else {
+                unwrapped = unwrapped.getCause();
+            }
+        }
 
-		return unwrapped;
-	}
+        return unwrapped;
+    }
 
-	public static Throwable unwrap(Throwable t,
-			Class<? extends Throwable> throwableClass) {
-		Throwable unwrapped = t;
+    public static Throwable unwrap(Throwable t,
+        Class<? extends Throwable> throwableClass) {
+        Throwable unwrapped = t;
 
-		while (unwrapped != null && throwableClass.isInstance(unwrapped)) {
-			if (unwrapped.getCause() == null
-					&& unwrapped instanceof InvocationTargetException) {
-				// We do this here because an invocation target exception may
-				// return null on getCause() but the throwable element we are
-				// looking for can be accessed via getTargetException()
-				unwrapped = ((InvocationTargetException) unwrapped)
-						.getTargetException();
-			} else {
-				unwrapped = unwrapped.getCause();
-			}
-		}
+        while (unwrapped != null && throwableClass.isInstance(unwrapped)) {
+            if (unwrapped.getCause() == null
+                && unwrapped instanceof InvocationTargetException) {
+                // We do this here because an invocation target exception may
+                // return null on getCause() but the throwable element we are
+                // looking for can be accessed via getTargetException()
+                unwrapped = ((InvocationTargetException) unwrapped)
+                    .getTargetException();
+            } else {
+                unwrapped = unwrapped.getCause();
+            }
+        }
 
-		return unwrapped;
-	}
+        return unwrapped;
+    }
 
-	private static boolean isInstance(Throwable t,
-			Class<? extends Throwable>[] throwableClasses) {
-		for (int i = 0; i < throwableClasses.length; i++) {
-			if (throwableClasses[i].isInstance(t)) {
-				return true;
-			}
-		}
+    private static boolean isInstance(Throwable t,
+        Class<? extends Throwable>... throwableClasses) {
+        for (int i = 0; i < throwableClasses.length; i++) {
+            if (throwableClasses[i].isInstance(t)) {
+                return true;
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
+
+    private static boolean equals(Class<? extends Throwable> t,
+        Class<? extends Throwable>... throwableClasses) {
+        for (int i = 0; i < throwableClasses.length; i++) {
+            if (throwableClasses[i].equals(t)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Gets the cause which are one of the given expected causes. This method is
+     * usefull when you have to find a cause within unexpected or unpredictable
+     * exception wrappings.
+     *
+     * @param e      the exception instance to search the cause on
+     * @param causes the causes which are searched.
+     * @return the found cause, where the first occurring cause instance will be returned. If
+     *         the cause could not be found, then null will be returned
+     */
+    public static Throwable getCause(Throwable e, Class<? extends Throwable>... causes) {
+        if ((e == null) || (causes == null) || (causes.length < 1)) {
+            return null;
+        } else if (isInstance(e, causes)) {
+            if (((e.getCause() == null) || (e.getCause()
+                .equals(e) || (!equals(e.getClass(), causes))))) {
+                return e;
+            } else {
+                return getCause(e.getCause(), causes);
+            }
+        } else if ((e.getCause() == null) && (e instanceof InvocationTargetException)) {
+            return getCause(((InvocationTargetException) e).getTargetException(), causes);
+        } else if (e.getCause() == null) {
+            return null;
+        } else {
+            return getCause(e.getCause(), causes);
+        }
+    }
 }
--- blaze-common-utils/src/test/java/com/blazebit/exception/ExceptionUtilsTest.java
+++ blaze-common-utils/src/test/java/com/blazebit/exception/ExceptionUtilsTest.java
@@ -3,53 +3,83 @@
  */
 package com.blazebit.exception;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 import java.lang.reflect.InvocationTargetException;
+import javax.management.RuntimeErrorException;
 
 import org.junit.Test;
 
 /**
- * 
+ *
  * @author Christian Beikov
  */
+@SuppressWarnings("unchecked")
 public class ExceptionUtilsTest {
 
-	/**
-	 * Test of unwrapInvocationTargetException method, of class ExceptionUtil.
-	 */
-	@Test
-	public void testUnwrapInvocationTargetException() {
-		Throwable inner = new RuntimeException();
-		Throwable outer = new InvocationTargetException(inner);
-		assertEquals(
-				ExceptionUtils.unwrap(outer, InvocationTargetException.class),
-				inner);
-		outer = new InvocationTargetException(outer);
-		assertEquals(
-				ExceptionUtils.unwrap(outer, InvocationTargetException.class),
-				inner);
-		assertEquals(
-				ExceptionUtils.unwrap(inner, InvocationTargetException.class),
-				inner);
-		assertNull(ExceptionUtils.unwrap(new InvocationTargetException(null),
-				InvocationTargetException.class));
-	}
+    /**
+     * Test of unwrapInvocationTargetException method, of class ExceptionUtil.
+     */
+    @Test
+    public void testUnwrapInvocationTargetException() {
+        Throwable inner = new RuntimeException();
+        Throwable outer = new InvocationTargetException(inner);
+        assertEquals(
+            ExceptionUtils.unwrap(outer, InvocationTargetException.class),
+            inner);
+        outer = new InvocationTargetException(outer);
+        assertEquals(
+            ExceptionUtils.unwrap(outer, InvocationTargetException.class),
+            inner);
+        assertEquals(
+            ExceptionUtils.unwrap(inner, InvocationTargetException.class),
+            inner);
+        assertNull(ExceptionUtils.unwrap(new InvocationTargetException(null),
+                                         InvocationTargetException.class));
+    }
 
-	/**
-	 * Test of unwrap method, of class ExceptionUtil.
-	 */
-	@Test
-	public void testUnwrap() {
-		Throwable inner = new RuntimeException();
-		Throwable outer = new InvocationTargetException(inner);
-		assertEquals(
-				ExceptionUtils.unwrap(outer, InvocationTargetException.class),
-				inner);
-		Throwable outer1 = new Error(outer);
-		assertEquals(ExceptionUtils.unwrap(outer1, Error.class), outer);
-		assertNull(ExceptionUtils.unwrap(inner, RuntimeException.class));
+    /**
+     * Test of unwrap method, of class ExceptionUtil.
+     */
+    @Test
+    public void testUnwrap() {
+        Throwable inner = new RuntimeException();
+        Throwable outer = new InvocationTargetException(inner);
+        assertEquals(
+            ExceptionUtils.unwrap(outer, InvocationTargetException.class),
+            inner);
+        Throwable outer1 = new Error(outer);
+        assertEquals(ExceptionUtils.unwrap(outer1, Error.class), outer);
+        assertNull(ExceptionUtils.unwrap(inner, RuntimeException.class));
 
-	}
+    }
+
+    @Test
+    public void testGetCause_null_exception() {
+        assertNull(ExceptionUtils.getCause(null, IllegalAccessError.class));
+    }
+
+    @Test
+    public void testGetCause_null_causes() {
+        assertNull(ExceptionUtils.getCause(new Exception(), (Class<Throwable>[]) null));
+    }
+
+    @Test
+    public void testGetCause_empty_causes() {
+        assertNull(ExceptionUtils.getCause(new Exception(), (Class<Throwable>[]) new Class[]{}));
+    }
+
+    @Test
+    public void testGetCause_not_found() {
+        final IllegalArgumentException expected = new IllegalArgumentException();
+        Exception ex = new Exception(new InvocationTargetException(expected));
+        assertNull(ExceptionUtils.getCause(ex, IllegalAccessError.class));
+    }
+
+    @Test
+    public void testGetCause() {
+        final IllegalArgumentException expected = new IllegalArgumentException();
+        Exception ex = new Exception(new InvocationTargetException(new Exception(expected)));
+        assertEquals(expected, ExceptionUtils.getCause(ex, IllegalAccessError.class, IllegalArgumentException.class));
+    }
 }
